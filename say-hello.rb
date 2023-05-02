86c11ff8b466af9619cf7d6134f18a9ffa2c8d4c
nevermore <nevermore@nevermoredeMac-mini.local>
2023-05-02 17:10:22 +0800

    add to homebrew

---
+++ say-hello.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SayHello < Formula
+    desc "这是一个测试程序"
+    homepage "https://github.com/Goooooooooogle/say-hello"
+    url "https://github.com/Goooooooooogle/say-hello/releases/download/v0.0.1/say-hello"
+    sha256 "5d0f2501e1aa8b22a2c40da19d9a587081b37e8ec1c7e92ccc3b88e8a239e2aa"
+    license ""
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      # system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      bin.install "say-hello"
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test say-hello`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
